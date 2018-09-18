08953c68f2d2f0fed2c95f36fbe375e754010034
Branko Culum <brankoculum@users.noreply.github.com>
2018-09-18 16:15:54 -0400

    Socket now points to localhost

--- src/App.js
+++ src/App.js
@@ -15,10 +15,7 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    const socket = new SockJS('https://rk-chat-app.herokuapp.com:9999/chat')
-    // const socket = new SockJS('https://rk-chat-app.herokuapp.com:9999/chat')
-    // const socket = new SockJS('http://plinkojs.com:9999/chat')
-
+    const socket = new SockJS('http://0.0.0.0:9999/chat');
 
     this.props.addSocket(socket);
 
